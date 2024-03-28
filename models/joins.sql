32006bf2a91d03a4b3d622ad63d9f424ecc67416
alinefranciolli@brq.com <alinefranciolli@brq.com>
2024-03-28 18:57:40 +0000

    .

---
+++ models/joins.sql
@@ -0,0 +1,16 @@
+with prod as (
+    select 
+ct.categoryname, 
+sp.companyname suppliers, 
+pd.productname, 
+pd.unitprice, 
+pd.productid
+from {{'dbt_dataset.products'}} pd 
+left join {{'dbt_dataset.suppliers'}} sp on (pd.supplierid = sp.supplierid)
+left join {{'dbt_dataset.categories'}} ct on (pd.categoryid = ct.categoryid)
+), orddetai as (
+    select pd.*, od.orderid, od.quantity, od.discount
+    from {{ref('orderdetails')}} od 
+    left join prod pd on (od.productid = pd.productid)
+)
+select * from orddetai
